bdb48c02123695efaa64b3e72e4d45921aa35d22
KlediCala1 <kledicala1@gmail.com>
2023-05-23 15:33:06 +0200

    commit

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        String name = "Kledi Cala";
+        int age = 19;
+        String hometown = "Elbasan";
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
